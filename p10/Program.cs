cc09f3843b03789f342bcd05d8da2586d0b9a4b8
PedroDutraSouza <147954687+PedroDutraSouza@users.noreply.github.com>
2025-06-30 15:25:17 +0000

    Add files via upload

---
+++ p10/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Pedro Dutra
+ * Date: 22/05/2025
+ * Time: 08:22
+ */
+using System;
+
+namespace p10
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Digite um número: ");
+			double num = Convert.ToDouble(Console.ReadLine());
+			
+			Console.Clear();
+			
+			if(num == 0)
+			{
+				Console.WriteLine("O número é igual a zero.");
+			}
+			else
+			{
+				Console.WriteLine("O número não e igual a zero.");
+			}
+	
+			Console.ReadKey(true);
+		}
+	}
+}
